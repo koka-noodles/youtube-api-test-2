ceacf3900965c91d0e5e4e5fd588d5addb977e1d
Lucas <lucasrowanwilson@gmail.com>
2015-05-06 15:02:36 +0100

    pulls one to five vids
    wont remove extra vids on a call that returns less results then before

--- search.js
+++ search.js
@@ -54,50 +54,78 @@ function onSearchResponse(response) { // stuckman
 
     
     var vnumber = response.pageInfo.totalResults;
+    
+    var capnum = 5;   // a cap on results delt with
 
-    console.log("vnumber"+vnumber);
+    if(vnumber > capnum){
+      vnumber = capnum;
+      console.log("I capped the results at "+capnum)
+    }
 
+    var pnumber = 2;
+   
+    console.log("total results = vnumber "+vnumber);
+  
     if(vnumber > 0){
 
-    var str = JSON.stringify(response.result);
-    $('#search-container').html('<pre>' + str + '</pre>');
+      var str = JSON.stringify(response.result);
+      $('#search-container').html('<pre>' + str + '</pre>');
 
-    vid = response.items[0].id.videoId;
-    vid2 = response.items[1].id.videoId;
-   
-    var count = Object.keys(response.items).length;
+      do {
+        var vindex = vnumber-1;
+        console.log("do loop vindex - the index of the array being pulled "+vindex);
+        console.log("do loop vnumber minus one "+vnumber);
 
-    // console.log(Object.keys(response)); // how meny results ?
+        //Make the up to five vids        
+        vid = response.items[vindex].id.videoId;  
 
-    console.log("stuckman results "+count); // this is a count of the keys not the number of results
+        player = new YT.Player('ytplayer'+vnumber, {
+          height: '390',
+          width: '640'
+        });
 
-    console.log("stuckman vid1: "+vid);
+        document.getElementById("ytplayer"+vnumber).src = "https://www.youtube.com/embed/"+vid;
 
-    console.log("stuckman vid2: "+vid2);
 
-    console.log("stuckman response items: "+response.items[0].id.videoId);
+        //incement down the capnum
+        vnumber --;
+      }
+      while(vnumber > 0); // the cap number is more then none
 
-    console.log("stuckman response totalresults count: "+response.pageInfo.totalResults);
+      
+      // vid = response.items[0].id.videoId;
+
+      // vid2 = response.items[1].id.videoId;
+     
+      // // var count = Object.keys(response.items).length;
+      // // // console.log(Object.keys(response)); // how meny results ?
+      // // console.log("stuckman results "+count); // this is a count of the keys not the number of results
+      // // console.log("stuckman vid1: "+vid);
+      // // console.log("stuckman vid2: "+vid2);
+      // // console.log("stuckman response items: "+response.items[0].id.videoId);
+      // // console.log("stuckman response totalresults count: "+response.pageInfo.totalResults);
+
+      
+
+      // player = new YT.Player('ytplayer', {
+      //   height: '390',
+      //   width: '640'
+      // });
+
+      // document.getElementById("ytplayer").src = "https://www.youtube.com/embed/"+vid;
 
     
 
-    player = new YT.Player('ytplayer', {
-      height: '390',
-      width: '640'
-    });
+      // player = new YT.Player('ytplayer'+pnumber, {
+      //   height: '390',
+      //   width: '640'
+      // });
+    
+      // document.getElementById("ytplayer"+pnumber).src = "https://www.youtube.com/embed/"+vid2;
+    }
 
-    document.getElementById("ytplayer").src = "https://www.youtube.com/embed/"+vid;
-
-  
-
-    player = new YT.Player('ytplayer2', {
-      height: '390',
-      width: '640'
-    });
-  
-    document.getElementById("ytplayer2").src = "https://www.youtube.com/embed/"+vid2;
-  }
   else{
+
     console.log("no results");
 
     document.getElementById("ytplayer").innerHTML = "<h2>No results</h2>"
@@ -120,5 +148,5 @@ var player;
 
 function reload(){
   console.log(player);
-  document.getElementById("ytplayer").src = "https://www.youtube.com/embed/"+vid2;
+  // document.getElementById("ytplayer").src = "https://www.youtube.com/embed/"+vid2;
 }
